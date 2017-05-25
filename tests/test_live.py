1c7cd18a0485b077eb9bf34f8d0731f9151ac245
Peter Harrington <pharrington@g.harvard.edu>
2017-05-25 16:58:40 -0700

    initial test for LiveTradingAlgorithm, not yet finished

--- tests/test_live.py
+++ tests/test_live.py
@@ -5,8 +5,15 @@ from unittest import TestCase
 import pandas as pd
 from datetime import time
 from collections import defaultdict
+from textwrap import dedent
 
 # fix to allow zip_longest on Python 2.X and 3.X
+from zipline.algorithm_live import LiveTradingAlgorithm
+from zipline.gens.brokers.broker import Broker
+from zipline.testing import ZiplineTestCase
+from zipline.testing import tmp_trading_env
+from zipline.testing.fixtures import WithSimParams
+
 try:                                    # Python 3
     from itertools import zip_longest
 except ImportError:                     # Python 2
@@ -14,11 +21,13 @@ except ImportError:                     # Python 2
 
 from mock import patch
 
-from zipline.gens.realtimeclock import RealtimeClock
+from zipline.gens.realtimeclock import RealtimeClock, SESSION_START
 from zipline.gens.sim_engine import MinuteSimulationClock
 
 from zipline.utils.calendars import get_calendar
 from zipline.utils.calendars.trading_calendar import days_at_time
+
+import zipline.protocol as zp
 
 
 class TestRealtimeClock(TestCase):
@@ -146,3 +155,100 @@ class TestRealtimeClock(TestCase):
         self.assertEquals(rtc_events[0], msc_events[0])  # Session start bar
         # BEFORE_TRADING_START is not fired as we're in mid-day
         self.assertEquals(rtc_events[1:], msc_events[msc_midday_position:])
+
+    def test_afterhours_start(self):
+        """Tests that RealtimeClock returns immediately if started after RTH"""
+        with patch('zipline.gens.realtimeclock.pd.to_datetime') as to_dt, \
+                patch('zipline.gens.realtimeclock.sleep') as sleep:
+            rtc = RealtimeClock(
+                self.sessions,
+                self.opens,
+                self.closes,
+                days_at_time(self.sessions, time(8, 45), "US/Eastern"),
+                False
+            )
+
+            to_dt.side_effect = self.get_clock
+            sleep.side_effect = self.advance_clock
+            self.internal_clock = pd.Timestamp("2017-04-20 20:05", tz='UTC')
+
+            events = list(rtc)
+            self.assertEquals(len(events), 1)
+
+            # Event 0 is SESSION_START which always triggered.
+            _, event_type = events[0]
+            self.assertEquals(event_type, SESSION_START)
+
+class TestBroker(Broker):
+
+    @property
+    def account(self):
+        return zp.Account()
+
+    @property
+    def time_skew(self):
+        return 0
+
+    def order(self, asset, amount, limit_price, stop_price, style):
+        raise NotImplementedError()
+
+    def get_open_orders(self, asset):
+        raise NotImplementedError()
+
+    def get_order(self, order_id):
+        raise NotImplementedError()
+
+    def cancel_order(self, order_param):
+        raise NotImplementedError()
+
+    def get_spot_value(self, assets, field, dt, data_frequency):
+        raise NotImplementedError()
+
+    @property
+    def portfolio(self):
+        return zp.Portfolio()
+
+    @property
+    def positions(self):
+        return zp.Positions()
+
+    def subscribe_to_market_data(self, symbol):
+        pass
+
+
+class TestStatePersistence(WithSimParams,
+                             ZiplineTestCase):
+    @classmethod
+    def init_class_fixtures(cls):
+        super(TestStatePersistence, cls).init_class_fixtures()
+        cls.futures_env = cls.enter_class_context(
+            tmp_trading_env(futures=cls.make_futures_info()),
+        )
+
+    def test_zipline_api_resolves_dynamically(self):
+        algo_code = dedent("""
+                from zipline.api import order, sid, record
+
+                def initialize(context):
+                    print "init has happened ***************************"
+                    context.ordered = False
+
+                def handle_data(context, data):
+                    if not context.ordered:
+                        order(sid(1), 1)
+                        context.ordered = True
+                """)
+
+        algo = LiveTradingAlgorithm(
+            script=algo_code,
+            sim_params=self.sim_params,
+            env=self.env,
+            trading_calendar=get_calendar("CME"),
+            broker=TestBroker()
+        )
+
+        algo.run()
+
+        # Verify that api methods get resolved dynamically by patching them out
+        # and then calling them
+        self.assertTrue(True)
